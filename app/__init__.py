71ae89a973f86f6415785162d96e621b6665d4da
Douglassdj <douglas.santosads@outlook.com>
2024-06-14 20:54:23 -0300

    Hioje

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,10 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "3364a1b3b9c120a257319a7b7c503903"
-app.config['SQLALCHEMY_DATABASE_URI'] = r'mssql+pyodbc://sa:Root1234@DOUG\SQLEXPRESS/PROJETOS?driver=ODBC+Driver+17+for+SQL+Server'
-
+# app.config['SQLALCHEMY_DATABASE_URI'] = r'mssql+pyodbc://sa:Root1234@DOUG\SQLEXPRESS/PROJETOS?driver=ODBC+Driver+17+for+SQL+Server'
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    r'mssql+pyodbc://@DOUGLAS\SQLEXPRESS/PROJETOS?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes'
+)
 
 
 database = SQLAlchemy(app)
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\dougl\OneDrive\Documentos\02 - Projetos\curso\venv
+set VIRTUAL_ENV=C:\Users\dougl\Documents\01 - Projetos\Financas\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
